5170de0de5835dc55eb35dc30b178b85383f81ca
gumraze-git <galaxydh4110@gmail.com>
2025-10-02 18:04:31 +0900

    feat: 대시보드 및 검색 페이지 라우팅 추가
    - `/dashboard`, `/player-search`, `/tournament-search`, `/court-manager` 경로 관련 라우팅 추가
    - 기본 경로를 `/dashboard`로 리다이렉트하도록 수정
    - `404` 경로 처리 방식 수정 (기본 경로로 리다이렉트)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import Login from "./pages/Login.tsx";
+import CourtManager from "./pages/Dashboard.tsx";
+import PlayerSearch from "./pages/search/PlayerSearch.tsx";
+import TournamentSearch from "./pages/search/TournamentSearch.tsx";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<CourtManager />} />
+        <Route path="/player-search" element={<PlayerSearch />} />
+        <Route path="/tournament-search" element={<TournamentSearch />} />
+        <Route path="/court-manager" element={<CourtManager />} />
         {/*없는 경로는 홈으로*/}
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<Navigate to="/dashboard" replace />} />
       </Routes>
     </BrowserRouter>
   );
